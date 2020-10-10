fc5961edc9e2ab315e03b3bc33a5b3e79f41fc26
marcosaleL <alelazarte55@gmail.com>
2020-10-10 20:02:07 -0300

    Prueba a la base de datos

---
+++ model/ModelProducto.php
@@ -0,0 +1,55 @@
+<?php
+
+// $host  = 'mysql:host=localhost;'.'dbname=VentaComidaRapida;charset=utf8';
+
+//class ModelProducto{
+    
+    /*private $db;
+
+    function __constructor(){
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=test;charset=utf8', 'root', '');
+    }
+    */
+    
+    //Funcion para obtener un arreglo de productos
+    function getProductos(){
+        $sentencia = $this->db->prepare("SELECT * FROM Producto");
+        $sentencia->execute();
+        $producto = $sentencia->fetchAll(PDO::FETCH_OBJ);
+        return $producto;
+    }
+    
+    //Funcion para obtener un producto con ID
+    function getProducto($id){
+        $sentencia = $this->db->prepare("SELECT * FROM Producto WHERE id_producto = ?");
+        $sentencia->execute(array($id));
+        $producto = $sentencia->fetch(PDO::FETCH_OBJ);
+        return $producto;
+    }
+
+    //Funcion para insertar a la tabla
+    function insertarProducto($nombre,$descripcion,$precio,$id_categoria){
+        $db = new PDO('mysql:host=localhost;'.'dbname=test;charset=utf8', 'root', '');
+        $sentencia = $db->prepare("INSERT INTO Producto(nombre,descripcion,precio,id_categoria) VALUES (" . "\"" . $nombre . "\",\"". $descripcion . "\"," . $precio . "," . $id_categoria . ")");
+        $sentencia->execute();
+    }
+
+    //Funcion para eliminar un producto
+    function deleteProducto($id){
+        $sentencia = $this->db->prepare("DELETE FROM Producto WHERE id_producto = ?");
+        $sentencia->execute(array($id));
+    }
+
+    //Funcion para update producto
+    function updateProducto($nombre, $descripcion, $precio, $id_categoria, $id_producto){
+        $sentencia = $this->db->prepare("UPDATE Producto SET nombre=" . "\"" . $nombre . "\", descripcion=" . "\"" . $descripcion . "\", precio=" . "\"" . $precio . "\", id_categoria=" . "\"" . $id_categoria . "\"" . "WHERE id_producto = $id_producto");
+        $sentencia->execute();
+    }
+
+//}
+    //$as = new ModelProducto();
+    //$as->insertarProducto("bcvd","esto son asdasas",29,1);
+    
+    insertarProducto("hola", "desc", 15, 1);
+    
+?>
