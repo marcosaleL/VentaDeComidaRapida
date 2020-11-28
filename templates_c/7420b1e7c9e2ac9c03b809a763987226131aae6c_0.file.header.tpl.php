c9c4508d26ddacc751c9180d375b2b66986feda3
marcosaleL <alelazarte55@gmail.com>
2020-11-28 08:11:28 -0300

    Archivos compilados, hay que corregir esto. El .gitignore no esta cumpliendo su trabajo

---
+++ templates_c/7420b1e7c9e2ac9c03b809a763987226131aae6c_0.file.header.tpl.php
@@ -0,0 +1,52 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-11-28 00:43:07
+  from 'C:\xampp\htdocs\TPEWeb2\templates\header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5fc18f0b09f542_98714351',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7420b1e7c9e2ac9c03b809a763987226131aae6c' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\TPEWeb2\\templates\\header.tpl',
+      1 => 1606519974,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5fc18f0b09f542_98714351 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <?php echo '<script'; ?>
+ src="./js/scripts.js"><?php echo '</script'; ?>
+>
+    <title><?php echo $_smarty_tpl->tpl_vars['titulo']->value;?>
+</title>
+</head>
+<body>
+    <header>
+         <!--<figure class="figureLogo">
+               <img class="imglogo" src="../images/logo2.png">
+         </figure>-->
+         <img class="imglogo" src="./images/logo2.png">
+        <nav>
+            <ul class="listaNav">
+                <li><a class="button" href="home">Inicio</a></li>
+                <li><a class="button" href="productos">Productos</a></li>
+                <li><a class="button" href="categorias">Categorias</a></li>
+                <li><a class="button" href="administracion">Administrador</a></li>
+            </ul>
+        </nav>
+    </header>
+<?php }
+}
